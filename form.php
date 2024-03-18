d2b6aabd39c73806b75067e6cba2bb00aeb6c17d
Masatoshi HANAI <masatoshi_hanai@centi-meter.net>
2024-03-18 18:32:39 +0900

    確認ページの追加

--- form.php
+++ form.php
@@ -25,19 +25,44 @@ if (!empty ($_POST['btn_confirm'])) {
 			<h2 class="text-center text-blue-400 font-bold text-2xl mb-10">お問合せフォーム</h2>
 			<div class="p-10 md:w-3/4 mx-auto lg:w-1/2">
 				<?php if ($page_flag === 1): ?>
+					<form method="post" action="">
+						<div class="mb-5">
+							<label for="" class="block mb-2 font-bold text-gray-600">氏名</label>
+							<p class="py-3 w-full mb-2">
+								<?php echo $_POST['your_name']; ?>
+							</p>
+						</div>
+
+						<div class="mb-5">
+							<label for="" class="block mb-2 font-bold text-gray-600">メールアドレス</label>
+							<p class="py-3 w-full mb-2">
+								<?php echo $_POST['email']; ?>
+							</p>
+						</div>
+						<div class="flex">
+							<div class="w-6/12 pr-3"><input type="submit" name="btn_back" value="戻る"
+									class="block w-full bg-gray-500 text-white font-bold p-4 rounded-lg"></div>
+							<div class="w-6/12 pl-3">
+								<input type="submit" name="btn_submit" value="送信"
+									class="block w-full bg-blue-500 text-white font-bold p-4 rounded-lg">
+							</div>
+						</div>
+						<input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
+						<input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
+					</form>
 
 				<?php else: ?>
 					<form method="post" action="">
 						<div class="mb-5">
 							<label for="" class="block mb-2 font-bold text-gray-600">氏名</label>
 							<input type="text" id="" name="your_name" placeholder=""
-								class="border border-gray-300 shadow p-3 w-full rounded mb-">
+								class="border border-gray-300 shadow p-3 w-full rounded mb-2">
 						</div>
 
 						<div class="mb-5">
 							<label for="" class="block mb-2 font-bold text-gray-600">メールアドレス</label>
 							<input type="text" id="" name="email" placeholder=""
-								class="border border-gray-300 shadow p-3 w-full rounded mb-">
+								class="border border-gray-300 shadow p-3 w-full rounded mb-2">
 						</div>
 
 						<input type="submit" name="btn_confirm" value="入力内容を確認する"
