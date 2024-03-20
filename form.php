30e9f91a8caeee8e2b384fd2d192af7b021ad9b6
Masatoshi HANAI <masatoshi_hanai@centi-meter.net>
2024-03-20 16:24:16 +0900

    ユーザーへの自動返信メールを追加

--- form.php
+++ form.php
@@ -1,5 +1,5 @@
 <?php
-var_dump($_POST);
+// var_dump($_POST);
 
 // 変数の初期化
 $page_flag = 0;
@@ -11,6 +11,33 @@ if (!empty ($_POST['btn_confirm'])) {
 } elseif (!empty ($_POST['btn_submit'])) {
 
 	$page_flag = 2;
+
+	mb_language("ja");
+	mb_internal_encoding("UTF-8");
+	// 変数とタイムゾーンを初期化
+	$header = null;
+	$auto_reply_subject = null;
+	$auto_reply_text = null;
+	date_default_timezone_set('Asia/Tokyo');
+
+	// ヘッダー情報を設定
+	$header = "MIME-Version: 1.0\n";
+	$header .= "From: センチメートル <masatoshi-hanai@centi_meter.net>\n";
+	$header .= "Reply-To: センチメートル <masatoshi-hanai@centi_meter.net>\n";
+
+	// 件名を設定
+	$auto_reply_subject = 'お問い合わせありがとうございます。';
+
+	// 本文を設定
+	$auto_reply_text = "この度は、お問い合わせ頂き誠にありがとうございます。\n
+		下記の内容でお問い合わせを受け付けました。\n\n";
+	$auto_reply_text .= "お問い合わせ日時：" . date("Y-m-d H:i") . "\n";
+	$auto_reply_text .= "氏名：" . $_POST['your_name'] . "\n";
+	$auto_reply_text .= "メールアドレス：" . $_POST['email'] . "\n\n";
+	$auto_reply_text .= "センチメートル";
+
+	// メール送信
+	mb_send_mail( $_POST['email'], $auto_reply_subject, $auto_reply_text, $header);
 
 }
 ?>
